ff6f83bd343576de8fd2abfb02b2f88dbb182146
Archana1422 <bolliarchana1422@gmail.com>
2026-05-24 12:24:18 +0530

    API Integration Project

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+
+response = requests.get(url)
+
+data = response.json()
+
+print("Bitcoin Price in USD:")
+print(data["bitcoin"]["usd"])
